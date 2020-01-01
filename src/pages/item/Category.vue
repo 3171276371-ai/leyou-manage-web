1cff7c40f7761a2a39a04f9155c7da85d6f01636
Administrator <3171276371@qq.com>
2020-01-01 21:58:06 +0800

    leyou-web

---
+++ src/pages/item/Category.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <v-tree url="/item/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+      },
+      handleDelete(id) {
+          // this.$message.confirm("确认要删除分组吗？")
+          // .then(() => {
+            this.$http.delete("/item/category/delete/" + id)
+                .then(() => {
+                    this.$message.success("删除成功");
+                    this.loadData();
+                })
+          // })
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
